25ff35d6f4cbaf033ca1e92ec68988f1fc3ef25d
AltynaiUrmatbekova <64131999+AltynaiUrmatbekova@users.noreply.github.com>
2022-03-04 13:02:14 +0600

    new scripts components

---
+++ script.js
@@ -0,0 +1,25 @@
+document.querySelectorAll('.accordion').forEach(accordion => {
+    const parent = accordion.closest('.accordions');
+  
+    accordion.querySelector('.accordion__header').addEventListener('click', function() {
+      if(parent && parent.classList.contains('accordions--toggle')) {
+        parent.querySelectorAll('.accordion').forEach(accordion => {
+          accordion.classList.remove("accordion--open");
+        });
+      }
+  
+      accordion.classList.add('accordion--open');
+    })
+  })
+  
+  document.querySelectorAll('[data-modal]').forEach(trigger => {
+    trigger.addEventListener('click', function() {
+      // console.log(trigger.dataset.modal);
+      document.getElementById(trigger.dataset.modal).classList.add('modal--open')
+    });
+  });
+  document.querySelectorAll('.modal').forEach(modal => {
+    modal.querySelector('.modal__window').addEventListener('click', function() {
+      modal.classList.remove('modal--open');
+    })
+  })
